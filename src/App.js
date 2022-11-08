dc397db22a6d65cd514146d29b2bd12690c01448
Jinnie-kim <hjk7639@gmail.com>
2022-11-08 13:25:47 +0900

    Feat: add photo close function

--- src/App.js
+++ src/App.js
@@ -36,6 +36,13 @@ function App() {
     setHasPhoto(true);
   };
 
+  const closePhoto = () => {
+    let photo = photoRef.current;
+    let ctx = photo.getContext('2d');
+    ctx.clearRect(0, 0, photo.width, photo.height);
+    setHasPhoto(false);
+  };
+
   useEffect(() => {
     getVideo();
   }, [videoRef]);
@@ -48,7 +55,7 @@ function App() {
       </div>
       <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
         <canvas ref={photoRef}></canvas>
-        <button>CLOSE!</button>
+        <button onClick={closePhoto}>CLOSE!</button>
       </div>
     </div>
   );
